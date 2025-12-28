374827072a8953707274cf5889d64e14293612cd
garimaallahbadia01 <garimaallahbadia01@gmail.com>
2025-12-28 21:25:06 +0530

    Set basename for BrowserRouter to '/deflated_affirmations'

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/deflated_affirmations">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
